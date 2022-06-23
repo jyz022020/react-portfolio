7562529abc48b63348133d895135e6aa94f8bfe3
Yunzhu Jin <jyz022020@gmail.com>
2022-06-23 11:14:24 -0700

    fix some bug and add link

--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -4,6 +4,22 @@ function Footer() {
     return (
         <footer>
             <h2>Created By Yunzhu Jin</h2>
+            <a href="https://github.com/jyz022020">
+                <img
+                    src={require(`../../assets/projects/github-logo.png`)}
+                    alt="github Repo"
+                    width="100" 
+                    height="100"
+                />
+            </a>
+            <a href="https://www.linkedin.com/in/kaylee-jin-3394821b6/">
+                <img
+                    src={require(`../../assets/projects/linkedin.png`)}
+                    alt="linkedin page"
+                    width="100" 
+                    height="100"
+                />
+            </a>
         </footer>
       );
 }
--- portfolio/src/components/Resume/index.js
+++ portfolio/src/components/Resume/index.js
@@ -3,9 +3,38 @@ function Resume() {
         <div>
             <div>
                 <span>Kaylee Jin's Resume  </span>
-                <a href={require(`../../assets/resume/resume.txt`)} download>
+                <a href={require(`../../assets/resume/resume.pdf`)} download>
                     Download the Resume
                 </a>
+                <div>
+                <h3>Yunzhu Jin</h3>
+                Phone: +1949-627-6677
+                E-mail: jyz022020@gmail.com
+                Personal portfolio: https://jyz022020.github.io/KJ-Portfolio/
+                Github Page: https://github.com/jyz022020
+                <p>Projects in Bootcamp</p>            
+                <p>UC Berkeley Extension Feb 2022-July 2022</p>
+                <p>Project 1 Night Out Planner</p>
+                <p>• Create an interactive and responsive application which has a polished UI.</p>
+                <p>• Use two server-side APIs.</p>    
+                <p>• Use Handlebars.js as the templating engine.</p>
+                <p>• Skills and technologies: deployment, client-side storage, interactivity, responsive design, polished UI.</p>    
+                <p>• Link: https://jyz022020.github.io/Project1-Night-Out-Planner/</p>   
+                <p>Project 2 Astrozod</p>
+                <p>• Use Node.js and Express.js to create a RESTful API.</p>   
+                <p>• Use Handlebars.js as the templating engine.</p>   
+                <p>• Use MySQL and the Sequelize ORM for the database.</p>    
+                <p>• Have both GET and POST routes for retrieving and adding new data.</p>    
+                <p>• Skills and technologies: server-side JavaScript, RESTful APIs, databases, objectrelational mapping, environment variables, templating, and authentication.</p>    
+                <p>• Deployed link: https://astrozod.herokuapp.com/</p>    
+                <p>Coding relative courses:</p> 
+                <p>University of Washington, Seattle</p>
+                <p>CSE 142: Computer Programming I </p>
+                <p>CSE 143: Computer Programming II </p>
+                </div>
+            </div>
+            <div>
+
             </div>
         </div>
     );
